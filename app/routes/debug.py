c5d44f9cb6c35d41e16a63c40580ef8d09c3a771
nox-vixen <nox.vixen.0@gmail.com>
2026-07-04 21:44:35 +0530

    Add debug watch endpoint

--- app/routes/debug.py
+++ app/routes/debug.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 import inspect
-from moviebox_api.v2._bases import BaseItemDetails
+from moviebox_api.v2 import DownloadableTVSeriesFilesDetail
 
 router = APIRouter()
 
 @router.get("/api/debug")
 async def debug():
     return {
-        "source": inspect.getsource(BaseItemDetails)
+        "source": inspect.getsource(DownloadableTVSeriesFilesDetail)
     }
