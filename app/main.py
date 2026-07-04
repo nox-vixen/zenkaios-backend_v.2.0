9759d9de8e9a8eb78d9fdacc9e3ac9dee327cd6c
nox-vixen <nox.vixen.0@gmail.com>
2026-07-04 22:15:00 +0530

    Remove debug route

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.home import router as home_router
 from app.routes.search import router as search_router
 from app.routes.details import router as details_router
-from app.routes.debug import router as debug_router
+
 from app.routes.episodes import router as episodes_router
-from app.routes.debug import router as debug_router
+
 from app.routes.watch import router as watch_router
 
 
@@ -28,9 +28,9 @@ app.add_middleware(
 app.include_router(home_router)
 app.include_router(search_router)
 app.include_router(details_router)
-app.include_router(debug_router)
+
 app.include_router(episodes_router)
-app.include_router(debug_router)
+
 app.include_router(watch_router)
 
 
