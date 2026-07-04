5cf22829fd361a072ed8fec48475c53567d49f2c
nox-vixen <nox.vixen.0@gmail.com>
2026-07-04 15:36:53 +0530

    Add debug watch endpoint

--- app/routes/debug.py
+++ app/routes/debug.py
@@ -1,15 +1,10 @@
 from fastapi import APIRouter
-
-from moviebox_api.v2 import TVSeriesDetails
-from app.providers.moviebox.client import session
+import moviebox_api.v2 as m
 
 router = APIRouter()
 
-
-@router.get("/debug/watch/{detail_path}")
-async def debug_watch(detail_path: str):
-    api = TVSeriesDetails(session)
-
-    data = await api.get_content_model(detail_path)
-
-    return data.model_dump()
+@router.get("/api/debug")
+async def debug():
+    return {
+        "members": dir(m)
+    }
